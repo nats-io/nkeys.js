6d5050f9cea67b5afc16b80037f77e306c2bfc41
Alberto Ricart <alberto@synadia.com>
2022-07-13 16:59:58 -0500

    [CHANGE] self processing the tweetnacl cjs module rather than use esm.sh as the size differential without shims is huge

--- modules/esm/deps.ts
+++ modules/esm/deps.ts
@@ -26,7 +26,7 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 //
 //   For more information, please refer to <http://unlicense.org>
-import nacl from "./tweetnacl.js";
+import { nacl } from "./nacl.js";
 
 import type { Ed25519Helper } from "../../src/helper.ts";
 
